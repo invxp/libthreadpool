99668675dac11c6d8b35335adf1033a144f9cc3e
invxp <invidentxp@gmail.com>
2014-12-29 14:37:45 +0800

    fix mirror bugs

--- test.cpp
+++ test.cpp
@@ -2,50 +2,166 @@
 #include <chrono>
 #include <ctime>
 #include <iostream>
+#include <atomic>
+#include <random>
+#include <string>
+#include <future>
+
+BrinK::pool::thread             tp__;
+std::mutex                      mutex__;
+volatile std::atomic_bool       exit_b__;
+volatile std::atomic_bool       exit_t__;
+std::atomic_uint64_t            count__;
 
 unsigned __int64 get_current_stamp()
 {
     return std::time(nullptr);
 }
 
-std::mutex mutex__;
-
 void test(const unsigned __int64& i)
 {
     std::unique_lock< std::mutex > lock(mutex__);
-
-    std::cout << "num : " << i << " thread_id : " << std::this_thread::get_id() << std::endl;
+    std::cout << "Num : " << i << std::endl;
 }
 
-int main(int,char**)
+void dispatch()
 {
+    std::unique_lock< std::mutex > lock(mutex__);
+    std::cout << "Dispatch Message" << std::endl;
+}
 
-    InvXp::threadpool::pool p;
+void random_test_broadcast()
+{
+    std::mt19937 engine(std::time(nullptr));
+    std::uniform_int_distribution<int> num(100, 200);
 
-    p.start();
+    static int send_count = 0;
 
-    for (int j=0;j<1;j++)
+    while (!exit_b__)
     {
-        int64_t dw = get_current_stamp();
-        for (int i=0;i<1000;i++)
-        {
-            p.post(std::bind(&test,i));
-        }
-        p.stop();
-        p.waitall();
-        std::cout << "Dispatch new message" << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        p.dispatch(std::bind(&test,99999999));
-        p.start();
-        p.waitall();
-
-        std::cout << "Post " << j << " completed " << get_current_stamp() - dw << std::endl;
-
+        int rand = num(engine);
+        tp__.post(std::bind(&test, ++count__));
+        std::this_thread::sleep_for(std::chrono::milliseconds(rand));
     }
-    int64_t dw = get_current_stamp();
-    p.stop();
 
-    std::cout << "Stop " << get_current_stamp() - dw << std::endl;
+}
+
+void random_test_start_stop()
+{
+    std::mt19937 rng(std::time(nullptr));
+    std::uniform_int<> ui(100, 200);
+
+    while (!exit_t__)
+    {
+        int rand = ui(rng);
+        tp__.start();
+        std::this_thread::sleep_for(std::chrono::milliseconds(rand));
+        tp__.stop();
+        tp__.cancel();
+    }
+}
+
+int main(int, char**)
+{
+    do
+    {
+        std::string cmd;
+
+        std::getline(std::cin, cmd);
+
+        if (cmd == "q")
+        {
+            std::cout << "stop pool" << std::endl;
+
+            std::async([]
+            {
+                tp__.stop();
+            });
+        }
+
+        else if (cmd == "s")
+        {
+            std::cout << "start 1 pool" << std::endl;
+
+            std::async([]
+            {
+                tp__.start(1);
+            });
+
+        }
+        else if (cmd == "p")
+        {
+            std::cout << "post 1" << std::endl;
+
+            std::async([]
+            {
+                tp__.post([]{});
+            });
+
+        }
+        else if (cmd == "t")
+        {
+            std::cout << "test start/stop" << std::endl;
+            exit_t__ = false;
+            std::async([]{random_test_start_stop(); });
+        }
+
+        else if (cmd == "w")
+        {
+            std::cout << "start post thread" << std::endl;
+            exit_b__ = false;
+            std::async([]
+            {
+                random_test_broadcast();
+            });
+        }
+        else if (cmd == "b")
+        {
+            std::cout << "broadcast msg" << std::endl;
+
+            std::async([]
+            {
+                for (auto i = 0; i < 10; ++i)
+                    tp__.dispatch(&dispatch);
+            });
+        }
+        else if (cmd == "a")
+        {
+            std::cout << "wait" << std::endl;
+
+            std::async([]
+            {
+                if (tp__.wait())
+                    std::cout << "wait_all_ok!!!" << std::endl;
+                else
+                    std::cout << "no_wait!!!" << std::endl;
+            });
+        }
+        else if (cmd == "o")
+        {
+            std::cout << "wait_one" << std::endl;
+
+            std::async([]
+            {
+                if (tp__.wait_one())
+                    std::cout << "wait_one_ok!!!" << std::endl;
+                else
+                    std::cout << "no_wait!!!" << std::endl;
+            });
+        }
+        else if (cmd == "qb")
+        {
+            std::cout << "stop broadcast" << std::endl;
+
+            exit_b__ = true;
+        }
+        else if (cmd == "qt")
+        {
+            std::cout << "stop stat/stop" << std::endl;
+
+            exit_t__ = true;
+        }
+    } while (true);
 
     system("pause");
 
--- threadpool.hpp
+++ threadpool.hpp
@@ -1,5 +1,5 @@
-#ifndef INVXP_THREAD_POOL_H
-#define INVXP_THREAD_POOL_H
+#ifndef BRINK_POOL_THREAD_H
+#define BRINK_POOL_THREAD_H
 
 #include <list>
 #include <thread>
@@ -7,161 +7,191 @@
 #include <memory>
 #include <condition_variable>
 #include <algorithm>
+#include <atomic>
 
-namespace InvXp
+namespace BrinK
 {
-    namespace threadpool
+    namespace pool
     {
-        typedef std::function< void() >                 task_t;
+        typedef std::function < void() >                 task_t;
 
-        typedef std::shared_ptr< std::thread >          thread_ptr_t;
+        typedef std::shared_ptr < std::thread >          thread_ptr_t;
 
-        class pool
+        class thread
         {
         public:
-            pool( const unsigned int& pool_size = std::thread::hardware_concurrency() );
-            virtual ~pool();
+            thread()
+            {
+                stop_flag_ = true;
+            }
+            virtual ~thread()
+            {
+                stop();
+                cancel();
+            }
 
         public:
-            void post( const task_t& f );
-            void dispatch( const task_t& f );
-            bool waitall();
-            void stop();
-            bool start( const unsigned int& pool_size = std::thread::hardware_concurrency() );
+            void post(const task_t& f)
+            {
+                std::unique_lock < std::mutex > lock(tasks_mutex_);
+                tasks_.push_back(f);
+                awake_thread_();
+            }
 
+            void dispatch(const task_t& f)
+            {
+                std::unique_lock < std::mutex > lock(tasks_mutex_);
+                tasks_.push_front(f);
+                awake_thread_();
+            }
+
+            bool wait()
+            {
+                {
+                    std::unique_lock < std::mutex > lock_stop(stop_mutex_);
+                    if (stop_flag_)
+                        return false;
+                }
+
+                std::unique_lock < std::mutex > lock_task(tasks_mutex_);
+                std::unique_lock < std::mutex > lock_wait(wait_all_mutex_);
+                awake_thread_(true);
+                lock_task.unlock();
+                wait_all_condition_.wait(lock_wait);
+
+                return true;
+            }
+
+            bool wait_one()
+            {
+                {
+                    std::unique_lock < std::mutex > lock_stop(stop_mutex_);
+                    if (stop_flag_)
+                        return false;
+                }
+
+                std::unique_lock < std::mutex > lock_task(tasks_mutex_);
+                std::unique_lock < std::mutex > lock_wait(wait_one_mutex_);
+                awake_thread_(true);
+                lock_task.unlock();
+                wait_one_condition_.wait(lock_wait);
+
+                return true;
+            }
+
+            void stop()
+            {
+                std::unique_lock < std::mutex > lock_stop(stop_mutex_);
+                std::unique_lock < std::mutex > lock_task(tasks_mutex_);
+                std::unique_lock < std::mutex > lock_all(wait_all_mutex_);
+                std::unique_lock < std::mutex > lock_one(wait_one_mutex_);
+
+                if (stop_flag_)
+                    return;
+
+                stop_flag_ = true;
+
+                lock_stop.unlock();
+
+                tasks_condition_.notify_all();
+
+                wait_all_condition_.notify_all();
+
+                wait_one_condition_.notify_all();
+
+                lock_task.unlock();
+
+                lock_all.unlock();
+
+                lock_one.unlock();
+
+                std::for_each(threads_.begin(), threads_.end(), [this](thread_ptr_t& thread){ thread->join(); });
+
+                threads_.clear();
+            }
+
+            bool start(const unsigned int& pool_size = std::thread::hardware_concurrency())
+            {
+                std::unique_lock < std::mutex > lock(stop_mutex_);
+
+                if (!stop_flag_)
+                    return false;
+
+                run_(pool_size);
+
+                return true;
+            }
+
+            void cancel()
+            {
+                std::unique_lock < std::mutex > lock(tasks_mutex_);
+                tasks_.clear();
+            }
+
+            size_t size()
+            {
+                std::unique_lock < std::mutex > lock(tasks_mutex_);
+                return tasks_.size();
+            }
         private:
-            void    pool_func_();
-            void    awake_thread_();
-            bool    get_task_( task_t& t );
-            void    clear_();
-            void    run_( const unsigned int & pool_size );
+            void pool_func_()
+            {
+                do
+                {
+                    std::unique_lock < std::mutex > lock_task(tasks_mutex_);
+                    std::unique_lock < std::mutex > lock_all(wait_all_mutex_);
+                    std::unique_lock < std::mutex > lock_one(wait_one_mutex_);
 
+                    if (stop_flag_)
+                        break;
+
+                    if (tasks_.empty())
+                    {
+                        wait_all_condition_.notify_all();
+                        lock_all.unlock();
+                        wait_one_condition_.notify_all();
+                        lock_one.unlock();
+                        tasks_condition_.wait(lock_task);
+                        continue;
+                    }
+
+                    tasks_.front()();
+                    tasks_.pop_front();
+                    wait_one_condition_.notify_all();
+                    lock_one.unlock();
+
+                    std::this_thread::yield();
+
+                } while (!stop_flag_);
+            }
+
+            void awake_thread_(const bool& all = false)
+            {
+                all ? tasks_condition_.notify_all() : tasks_condition_.notify_one();
+            }
+
+            void run_(const unsigned int & pool_size)
+            {
+                stop_flag_ = false;
+                for (unsigned int i = 0; i < pool_size; i++)
+                    threads_.emplace_back(std::make_shared<std::thread>(std::bind(&BrinK::pool::thread::pool_func_, this)));
+            }
         private:
-
-            std::list< task_t >                                     tasks_;
+            std::list < task_t >                                    tasks_;
             std::mutex                                              tasks_mutex_;
+            std::condition_variable                                 tasks_condition_;
 
-            std::list< thread_ptr_t >                               threads_;
+            std::list < thread_ptr_t >                              threads_;
 
-            std::condition_variable                                 condition_;
+            volatile std::atomic_bool                               stop_flag_;
+            std::mutex                                              stop_mutex_;
 
             std::mutex                                              wait_all_mutex_;
             std::condition_variable                                 wait_all_condition_;
 
-            volatile    bool                                        stop_flag_;
+            std::mutex                                              wait_one_mutex_;
+            std::condition_variable                                 wait_one_condition_;
         };
-
-        pool::pool( const unsigned int& pool_size ) :stop_flag_(false)
-        {
-            run_(pool_size);
-        }
-
-        pool::~pool()
-        {
-            clear_();
-        }
-
-        void pool::post( const task_t& f )
-        {
-            std::unique_lock< std::mutex > lock(tasks_mutex_);
-            tasks_.push_back(f);
-            lock.unlock();
-            awake_thread_();
-        }
-
-        void pool::dispatch( const task_t& f )
-        {
-            std::unique_lock< std::mutex > lock(tasks_mutex_);
-            tasks_.push_front(f);
-            lock.unlock();
-
-            awake_thread_();
-        }
-
-        bool pool::get_task_( task_t& t )
-        {
-            std::unique_lock< std::mutex > lock(tasks_mutex_);
-
-            while (tasks_.empty())
-            {
-                wait_all_condition_.notify_all();
-
-                if (stop_flag_)
-                {
-                    t = []{};
-                    return false;
-                }
-
-                condition_.wait(lock);
-            }
-
-            t = tasks_.front();
-            tasks_.pop_front();
-            return true;
-        }
-
-        bool pool::waitall()
-        {
-            if (stop_flag_)
-                return false;
-
-            std::unique_lock< std::mutex > lock(wait_all_mutex_);
-            wait_all_condition_.wait(lock);
-            return true;
-        }
-
-        void pool::pool_func_()
-        {
-            while (!stop_flag_)
-            {
-                task_t task;
-                get_task_(task);
-                task();
-                std::this_thread::yield();
-            }
-        }
-
-        void pool::awake_thread_()
-        {
-            condition_.notify_one();
-        }
-
-        void pool::stop()
-        {
-            stop_flag_ = true;
-
-            condition_.notify_all();
-
-            std::for_each(threads_.begin(), threads_.end(), [this]( thread_ptr_t& thread ){ thread->join(); });
-
-            threads_.clear();
-
-        }
-
-        void pool::clear_()
-        {
-            threads_.clear();
-            std::unique_lock< std::mutex > lock(tasks_mutex_);
-            tasks_.clear();
-        }
-
-        bool pool::start( const unsigned int& pool_size /*= std::thread::hardware_concurrency() */ )
-        {
-            if (!stop_flag_)
-                return false;
-
-            run_(pool_size);
-
-            return true;
-        }
-
-        void pool::run_( const unsigned int & pool_size )
-        {
-            stop_flag_ = false;
-            for (unsigned int i = 0; i < pool_size; i++)
-            threads_.emplace_back(std::make_shared<std::thread>(std::bind(&threadpool::pool::pool_func_, this)));
-        }
-
 
     }
 }
